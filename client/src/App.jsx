c85188636ea461d11e86820b822ed4cc9acbf173
Martin LoFaso <marty@Martins-Air.attlocal.net>
2024-06-06 20:44:08 -0700

    succes page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,25 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet } from "react-router-dom";
 import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
   createHttpLink,
-} from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
-import { StoreProvider } from './utils/globalState';
-import './App.css';
-import NavBar from './components/Nav-bar';
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { StoreProvider } from "./utils/globalState";
+import "./App.css";
+import NavBar from "./components/Nav-bar";
 
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  uri: "/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('id_token');
+  const token = localStorage.getItem("id_token");
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : '',
+      authorization: token ? `Bearer ${token}` : "",
     },
   };
 });
